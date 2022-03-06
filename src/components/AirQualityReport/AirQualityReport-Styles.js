7747269efaec05fbffc3e298fdb94a2d213dd459
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-03-06 12:35:23 -0800

    initial commit

---
+++ src/components/AirQualityReport/AirQualityReport-Styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Main = styled("button")`
+    width: 362px;
+    max-width: 100%;
+    z-index: 500;
+    /* left: 72px; */
+    position: relative;
+    background: #fff;
+    border-radius: 6px;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 24%);
+    padding: 16px;
+    /* display: -ms-flexbox; */
+    display: flex;
+    /* -ms-flex-direction: column; */
+    flex-direction: column;
+    /* -ms-flex-pack: start; */
+    justify-content: flex-start;
+    /* -ms-flex-align: start; */
+    align-items: flex-start;
+`;
---
+++ src/components/AirQualityReport/AirQualityReport.js
@@ -0,0 +1,8 @@
+import { Main } from "./AirQualityReport-Styles";
+
+export default function AirQualityReport({ locData }) {
+
+return <div className={Main}>
+AirQreport
+</div>  
+}
